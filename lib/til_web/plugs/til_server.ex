aeca6d344b8de0eb60d4263475a7b04ddb197033
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-12-10 21:37:08 +0530

    redesign the til home

--- lib/til_web/plugs/til_server.ex
+++ lib/til_web/plugs/til_server.ex
@@ -10,20 +10,20 @@ defmodule TilWeb.Plugs.TilServerPlug do
 
   def call(conn, _opts) do
     # get subdomain
-    with {:ok, user_id} <- get_user(conn),
+    with {:ok, user} <- get_user(conn),
          # get path
          {:ok, path} <- get_path(conn) do
-      render_post(conn, user_id, path)
+      render_post(conn, user, path)
     end
   end
 
   # render home page
-  defp render_post(conn, user_id, "") do
+  defp render_post(conn, user, "") do
     posts =
-      from(p in Post, where: p.user_id == ^user_id, order_by: [desc: p.inserted_at], limit: 10)
+      from(p in Post, where: p.user_id == ^user.id, order_by: [desc: p.inserted_at], limit: 10)
       |> Repo.all()
 
-    {:safe, iodata} = Phoenix.View.render(TilServerView, "index.html", posts: posts)
+    {:safe, iodata} = Phoenix.View.render(TilServerView, "index.html", posts: posts, user: user)
 
     conn
     |> put_resp_content_type("text/html")
@@ -31,9 +31,9 @@ defmodule TilWeb.Plugs.TilServerPlug do
   end
 
   # render home page
-  defp render_post(conn, user_id, path) do
+  defp render_post(conn, user, path) do
     # find the til
-    with {:ok, post} <- get_post(user_id, path) do
+    with {:ok, post} <- get_post(user, path) do
       {:safe, iodata} = Phoenix.View.render(TilServerView, "post.html", post: post)
       # render it
       conn
@@ -46,8 +46,8 @@ defmodule TilWeb.Plugs.TilServerPlug do
   defp get_user(conn) do
     subdomain = conn.host |> to_string |> String.replace_suffix(@host_suffix, "")
 
-    case Repo.all(from(u in User, where: u.github_username == ^subdomain, select: u.id, limit: 1)) do
-      [user_id] -> {:ok, user_id}
+    case Repo.all(from(u in User, where: u.github_username == ^subdomain)) do
+      [user] -> {:ok, user}
       _ -> :site_not_found
     end
   end
@@ -61,10 +61,10 @@ defmodule TilWeb.Plugs.TilServerPlug do
         |> String.replace_suffix("/", "")
       }
 
-  defp get_post(user_id, path) do
+  defp get_post(user, path) do
     path = path <> ".md"
 
-    case from(p in Post, where: p.path == ^path and p.user_id == ^user_id)
+    case from(p in Post, where: p.path == ^path and p.user_id == ^user.id)
          |> Repo.all() do
       [] -> :post_not_found
       [post] -> {:ok, post}
--- lib/til_web/views/til_server_view.ex
+++ lib/til_web/views/til_server_view.ex
@@ -1,5 +1,7 @@
 defmodule TilWeb.Views.TilServerView do
   use TilWeb, :view
+  alias Til.Accounts.User
+  alias Til.Repository.Post
 
   defp markdown_to_html(body) do
     {:ok, html, []} =
@@ -9,5 +11,18 @@ defmodule TilWeb.Views.TilServerView do
       })
 
     {:safe, html}
+  end
+
+  def user_label(nil), do: ""
+  def user_label(%User{} = user), do: user.name || user.github_username
+
+  def avatar(%User{avatar_url: avatar_url})
+      when not is_nil(avatar_url) and avatar_url != "",
+      do: avatar_url
+
+  def avatar(_), do: "https://www.gravatar.com/avatar/?d=identicon"
+
+  def post_url(%Post{} = post) do
+    post.path |> String.replace_suffix(".md", "")
   end
 end
