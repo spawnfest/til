335191824311f129010b53290af45dea69ec4c95
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-12-10 20:17:49 +0530

    remove demo code from dash

--- lib/til_web/controllers/dash_controller.ex
+++ lib/til_web/controllers/dash_controller.ex
@@ -3,7 +3,6 @@ defmodule TilWeb.DashController do
 
   def index(conn, _params) do
     posts = Repository.posts_for(conn.assigns[:current_user])
-    posts = Enum.flat_map(1..12, fn _ -> posts end)
     render(conn, posts: posts)
   end
 end
