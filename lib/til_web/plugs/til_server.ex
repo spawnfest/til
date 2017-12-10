17d5f6e8e1174bd5e92d63b6acbf2048fbb86ed6
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-12-10 14:31:15 +0530

    render post

--- lib/til_web/plugs/til_server.ex
+++ lib/til_web/plugs/til_server.ex
@@ -2,6 +2,7 @@ defmodule TilWeb.Plugs.TilServerPlug do
   import Plug.Conn
   import Phoenix.Controller
   alias Til.Repo
+  alias Til.Repository.Post
   import Ecto.Query
   alias Til.Accounts.User
 
@@ -9,33 +10,62 @@ defmodule TilWeb.Plugs.TilServerPlug do
 
   def call(conn, _opts) do
     # get subdomain
-    with {:ok, subdomain} <- get_subdomain(conn),
+    with {:ok, user_id} <- get_user(conn),
          # get path
-         {:ok, path} <- get_path(conn),
-         # find the til
-         {:ok, til} <- get_til(subdomain, path) do
+         {:ok, path} <- get_path(conn) do
+      render_post(conn, user_id, path)
+    end
+  end
+
+  # render home page
+  defp render_post(conn, user_id, "") do
+    conn
+    |> put_resp_content_type("text/html")
+    |> send_resp(:ok, "<!doctype html><h1>Home Page</h1>")
+  end
+
+  # render home page
+  defp render_post(conn, user_id, path) do
+    # find the til
+    with {:ok, post} <- get_post(user_id, path) do
       # render it
       conn
       |> put_resp_content_type("text/html")
-      |> send_resp(:ok, "<!doctype html><h1>Awesome</h1>")
+      |> send_resp(:ok, "<!doctype html># #{post.title}\n#{post.body}")
     end
   end
 
   # TODO: move to config
   @host_suffix ".tilhub.in"
-  defp get_subdomain(conn) do
+  defp get_user(conn) do
     subdomain = conn.host |> to_string |> String.replace_suffix(@host_suffix, "")
 
-    case Repo.all(from(u in User, where: u.username == ^subdomain, select: 1, limit: 1)) do
-      [_] -> {:ok, subdomain}
+    case Repo.all(from(u in User, where: u.github_username == ^subdomain, select: u.id, limit: 1)) do
+      [user_id] -> {:ok, user_id}
       _ -> :site_not_found
     end
   end
 
-  defp get_path(conn), do: {:ok, conn.path |> Path.join()}
+  defp get_path(conn),
+    do:
+      {
+        :ok,
+        conn.path_info
+        |> Enum.join("/")
+        |> String.replace_suffix("/", "")
+      }
+      |> IO.inspect(label: "PATH")
 
-  defp get_til(subdomain, path) do
-    nil
-    # Repo.all(from p in Post, where: p.path == path and p.subdomain == subdomain)
+  defp get_post(user_id, "") do
+  end
+
+  defp get_post(user_id, path) do
+    path = path <> ".md"
+
+    case from(p in Post, where: p.path == ^path and p.user_id == ^user_id)
+         |> Repo.all() do
+      [] -> :post_not_found
+      [post] -> {:ok, post}
+    end
   end
 end
